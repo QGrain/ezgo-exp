fa6b4170af70ce4a50ad18c98e2afed58089c0d8
QGrain <zhiyuzhang1999@163.com>
2023-04-22 20:53:41 +0800

    for uuid

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
-/*
-#cgo linux CFLAGS: -fplugin=./poc.so
-#include <stdio.h>
+import "fmt"
 
-void hello_world(){
-    printf("hello world\n");
-}
-*/
-import "C"
+// /*
+// #cgo linux CFLAGS: -fplugin=./poc.so
+// #include <stdio.h>
+
+// void hello_world(){
+//     printf("hello world\n");
+// }
+// */
+// import "C"
+
+// func main() {
+// 	C.hello_world()
+// }
 
 func main() {
-	C.hello_world()
+	fmt.Println("This is nothing, just for uuid.")
 }
